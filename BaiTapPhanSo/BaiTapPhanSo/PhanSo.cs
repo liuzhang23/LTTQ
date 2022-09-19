e384573b958ba212e20950bd22c391ba238e95b5
lepro83 <72549881+lepro83@users.noreply.github.com>
2022-09-19 08:54:48 +0700

    first commit

---
+++ BaiTapPhanSo/BaiTapPhanSo/PhanSo.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaiTapPhanSo
+{
+    class Phanso
+    {
+        int tu, mau;
+        public Phanso()
+        {
+            tu = 0;
+            mau = 1;
+        }
+        public Phanso(int x)
+        {
+            tu = x;
+            mau = 1;
+        }
+        public Phanso(int t, int m)
+        {
+            tu = t;
+            mau = m;
+        }
+        public void NhapPhanso()
+        {
+            Console.Write("Tu so = ");
+            this.tu = int.Parse(Console.ReadLine());
+            Console.Write("mau so = ");
+            do
+                this.mau = int.Parse(Console.ReadLine());
+            while (this.mau == 0);
+        }
+        public void XuatPhanSo()
+        {
+            Console.WriteLine("{0}/{1}", tu, mau);
+        }
+        public Phanso CongPS(Phanso ps1)
+        {
+            int ts = tu * ps1.mau + mau * ps1.tu;
+            int ms = mau * ps1.mau;
+            Phanso ketqua = new Phanso(ts, ms);
+            return ketqua;
+        }
+        public Phanso TruPS(Phanso ps1)
+        {
+            int ts = tu * ps1.mau - mau * ps1.tu;
+            int ms = mau * ps1.mau;
+            Phanso ketqua = new Phanso(ts, ms);
+            return ketqua;
+        }
+
+        public Phanso NhanPS(Phanso ps1)
+        {
+            int ts = tu * ps1.tu;
+            int ms = mau * ps1.mau;
+            Phanso ketqua = new Phanso(ts, ms);
+            return ketqua;
+        }
+        public Phanso ChiaPS(Phanso ps1)
+        {
+            int ts = tu * ps1.mau;
+            int ms = mau * ps1.tu;
+            Phanso ketqua = new Phanso(ts, ms);
+            return ketqua;
+        }
+        public int UCLN()
+        {
+            while (tu != mau)
+                if (tu > mau) tu = tu - mau;
+                else mau = mau - tu;
+            return tu;
+        }
+
+        public Phanso RutGon()
+        {
+            int ts = tu;
+            int ms = mau;
+            while (tu != mau)
+                if (tu > mau) tu = tu - mau;
+                else mau = mau - tu;
+            ts = ts / tu; ms = ms / tu;
+            Phanso ketqua = new Phanso(ts, ms);
+            return ketqua;
+        }
+
+    }
+}
---
+++ BaiTapPhanSo/BaiTapPhanSo/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaiTapPhanSo
+{
+    class Program
+
+    {
+
+        static void Main(string[] args)
+        {
+            int chon, a, d, c;
+            Phanso ps1 = new Phanso();
+            Phanso ps2 = new Phanso();
+            Phanso ps3 = new Phanso();
+            do
+            {
+                Console.WriteLine();
+                Console.WriteLine("**************************MENU************************");
+                Console.WriteLine("   1. Tong");
+                Console.WriteLine("   2. Hieu");
+                Console.WriteLine("   3. Tich");
+                Console.WriteLine("   4. Thuong");
+                Console.WriteLine("   5. Kiem tra 1 phan so toi gian");
+                Console.WriteLine("   6. Rut gon phan so");
+                Console.WriteLine("   7. Thoat");
+                chon = Int32.Parse(Console.ReadLine());
+                switch (chon)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Nhap phan so 1");
+                            ps1.NhapPhanso();
+                            Console.WriteLine("Nhap phan so 2");
+                            ps2.NhapPhanso();
+                            ps3 = ps1.CongPS(ps2);
+                            Console.Write("Tong cua hai phan so: ");
+                            ps3.XuatPhanSo();
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("Nhap phan so 1");
+                            ps1.NhapPhanso();
+                            Console.WriteLine("Nhap phan so 2");
+                            ps2.NhapPhanso();
+                            ps3 = ps1.TruPS(ps2);
+                            Console.Write("Hieu cua hai phan so :");
+                            ps3.XuatPhanSo();
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("Nhap phan so 1");
+                            ps1.NhapPhanso();
+                            Console.WriteLine("Nhap phan so 2");
+                            ps2.NhapPhanso();
+                            ps3 = ps1.NhanPS(ps2);
+                            Console.Write("Tich cua hai phan so :");
+                            ps3.XuatPhanSo();
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.WriteLine("Nhap phan so 1");
+                            ps1.NhapPhanso();
+                            Console.WriteLine("Nhap phan so 2");
+                            ps2.NhapPhanso();
+                            ps3 = ps1.ChiaPS(ps2);
+                            Console.Write("Hieu cua hai phan so :");
+                            ps3.XuatPhanSo();
+                            break;
+                        }
+                    case 5:
+                        {
+                            Console.WriteLine("Nhap phan so can kiem tra");
+                            ps1.NhapPhanso();
+                            if (ps1.UCLN() > 1)
+                            {
+                                Console.WriteLine("Phan so chua toi gian");
+                            } else
+                            {
+                                Console.WriteLine("Phan so da toi gian");
+                            }
+                            break;
+                        }
+                    case 6:
+                        {
+                            Console.WriteLine("Nhap phan so can rut gon");
+                            ps1.NhapPhanso();
+                            ps3 = ps1.RutGon();
+                            Console.Write("Phan so sau khi rut gon: ");
+                            ps3.XuatPhanSo();
+                            break;
+                        }
+                    case 7: goto ketthuc;
+                }
+            }
+            while (chon != 7);
+
+            Console.ReadLine();
+
+        ketthuc:;
+
+        }
+
+    }
+
+}
+
